6f720d209332d4e89445ceffe51bf2b86ee58eb7
ArmaanZabihi <armaanzabihi@gmail.com>
2023-01-19 16:31:15 -0800

    note taker app

--- server.js
+++ server.js
@@ -1,25 +1,69 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const shortid = require('shortid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method}`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const titleText = {
+      title,
+      text,
+      id:shortid.generate(),
+     
+    };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(titleText);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully saved note!")
+        );
+      }
+    });
+    const response = {
+      status: "success",
+      body: titleText,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
